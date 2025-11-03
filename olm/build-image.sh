be6eda9bd7f1b44ccba7ea7251e7473088699098
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-11-03 09:50:04 -0500

    dump out repo file setup

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -17,6 +17,7 @@ if [ -f "$repo_file" ]; then
 	echo 'priority=1' >> "$repo_filename"
 	echo 'enabled=1' >> "$repo_filename"
 fi
+cat "$repo_file"
 
 for component in acmesolver cainjector controller startupapicheck webhook; do
 	name="cert-manager-${component}"
