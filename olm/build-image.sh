36c77b3c0174444f70b50b505fe0edba747f49db
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-11-04 12:49:18 -0500

    mount yum.conf and yum.repos.internal.d instead

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -10,16 +10,13 @@ version="1.19.1"
 registry="container-registry.oracle.com/olcne"
 git_commit="$(git rev-parse --short HEAD)"
 
-# Temp change
-ls -ltrh /etc/yum.repos.d
-rm /etc/yum.repos.d/ol_artifacts.repo
-
 for component in acmesolver cainjector controller startupapicheck webhook; do
 	name="cert-manager-${component}"
 	docker_tag="${registry}/${name}:${version}"
 	"${CONTAINER_CLI}" build --pull \
 		--build-arg https_proxy="${https_proxy}" \
-		--volume /etc/yum.repos.d:/etc/yum.repos.d \
+		--volume /etc/yum.repos.internal.d:/etc/yum.repos.internal.d \
+		--volume /etc/yum.conf:/etc/yum.conf \
 		--build-arg VERSION="${version}" \
 		--build-arg GIT_COMMIT="${git_commit}" \
 		-t ${docker_tag} -f ./olm/builds/Dockerfile."${component}" .
