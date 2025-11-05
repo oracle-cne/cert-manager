23c93fcf0a7afe18be84a808274e5a78e1c4d21a
Michael Gianatassio <michael.gianatassio@oracle.com>
2025-11-05 09:58:52 -0500

    sync with changes to build 1.19.1

--- olm/build-image.sh
+++ olm/build-image.sh
@@ -10,19 +10,13 @@ version="v{{{ .major }}}.{{{ .minor }}}.{{{ .patch }}}"
 registry="container-registry.oracle.com/olcne"
 git_commit="$(git rev-parse --short HEAD)"
 
-repo_filename="ol_artifacts.repo"
-repo_file="/etc/yum.repos.d/$repo_filename"
-if [ -f "$repo_file" ]; then
-	cp "$repo_file" ./
-	echo 'priority=1' >> "$repo_filename"
-	echo 'enabled=1' >> "$repo_filename"
-fi
-
 for component in acmesolver cainjector controller startupapicheck webhook; do
 	name="cert-manager-${component}"
 	docker_tag="${registry}/${name}:${version}"
 	"${CONTAINER_CLI}" build --pull \
 		--build-arg https_proxy="${https_proxy}" \
+		--volume /etc/yum.repos.internal.d:/etc/yum.repos.internal.d \
+		--volume /etc/yum.conf:/etc/yum.conf \
 		--build-arg VERSION="${version}" \
 		--build-arg GIT_COMMIT="${git_commit}" \
 		-t ${docker_tag} -f ./olm/builds/Dockerfile."${component}" .
